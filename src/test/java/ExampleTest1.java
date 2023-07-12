75b528e8371447aa7a32fd810e41c359f4bb6a6a
Tarushii <tishi.tng@gmail.com>
2023-07-11 17:04:48 -0700

    initial commit

---
+++ src/test/java/ExampleTest1.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class ExampleTest1 {
+  @Test
+  public void test1() {
+    new Example().doSomething(1);
+  }
+}
---
+++ src/test/java/ExampleTest2.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class ExampleTest2 {
+  @Test
+  public void test2() {
+    new Example().doSomething(2);
+  }
+}
