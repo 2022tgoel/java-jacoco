0efcf0cd1247f004280f384b5fbc26af6ff77849
Tarushii <tishi.tng@gmail.com>
2023-07-12 21:42:35 -0700

    fix test names

---
+++ src/test/java/TestExample1.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class TestExample1 {
+  @Test
+  public void test1() {
+    new Example().doSomething(1);
+  }
+}
---
+++ src/test/java/TestExample2.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class TestExample2 {
+  @Test
+  public void test2() {
+    new Example().doSomething(2);
+  }
+}
